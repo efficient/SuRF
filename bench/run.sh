eecb78113e124457e02bbd285efae4a555f3e9c2
Huanchen Zhang <huanchenzhang.cs@gmail.com>
2018-04-19 16:36:51 -0400

    modifed random int benchmarks

--- bench/run.sh
+++ bench/run.sh
@@ -16,25 +16,25 @@ echo 'SuRFMixed, 2-bit hash suffixes and 2-bit real suffixes, random int, point 
 ../build/bench/workload SuRFMixed 2 mixed 50 0 randint mix zipfian
 
 
-echo 'Bloom Filter, email, point queries'
-../build/bench/workload Bloom 1 mixed 50 0 email point zipfian
+# echo 'Bloom Filter, email, point queries'
+# ../build/bench/workload Bloom 1 mixed 50 0 email point zipfian
 
-echo 'SuRF, email, point queries'
-../build/bench/workload SuRF 1 mixed 50 0 email point zipfian
+# echo 'SuRF, email, point queries'
+# ../build/bench/workload SuRF 1 mixed 50 0 email point zipfian
 
-echo 'SuRFHash, 4-bit suffixes, email, point queries'
-../build/bench/workload SuRFHash 4 mixed 50 0 email point zipfian
+# echo 'SuRFHash, 4-bit suffixes, email, point queries'
+# ../build/bench/workload SuRFHash 4 mixed 50 0 email point zipfian
 
-echo 'SuRFReal, 4-bit suffixes, email, point queries'
-../build/bench/workload SuRFReal 4 mixed 50 0 email point zipfian
+# echo 'SuRFReal, 4-bit suffixes, email, point queries'
+# ../build/bench/workload SuRFReal 4 mixed 50 0 email point zipfian
 
-echo 'SuRFMixed, 2-bit hash suffixes and 2-bit real suffixes, email, point queries'
-../build/bench/workload SuRFMixed 2 mixed 50 0 email mix zipfian
+# echo 'SuRFMixed, 2-bit hash suffixes and 2-bit real suffixes, email, point queries'
+# ../build/bench/workload SuRFMixed 2 mixed 50 0 email mix zipfian
 
 
 echo 'SuRFReal, 4-bit suffixes, random int, point queries'
 ../build/bench/workload SuRFReal 4 mixed 50 0 randint range zipfian
 
-echo 'SuRFReal, 4-bit suffixes, email, point queries'
-../build/bench/workload SuRFReal 4 mixed 50 0 email range zipfian
+# echo 'SuRFReal, 4-bit suffixes, email, point queries'
+# ../build/bench/workload SuRFReal 4 mixed 50 0 email range zipfian
 
--- bench/workload_gen/gen_workload.sh
+++ bench/workload_gen/gen_workload.sh
@@ -5,9 +5,9 @@ python gen_txn.py randint uniform
 python gen_txn.py randint zipfian
 #python gen_txn.py randint latest
 
-python gen_load.py email uniform
-python gen_txn.py email uniform
-python gen_txn.py email zipfian
+#python gen_load.py email uniform
+#python gen_txn.py email uniform
+#python gen_txn.py email zipfian
 #python gen_txn.py email latest
 
 
